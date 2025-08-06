659677a498b3775c16f9daacf9a7aa995ee6bec0
iddadda <iddadda241@gmail.com>
2025-08-06 17:11:11 +0900

    [UserSignUp] 기능 구현, 예외, Validation 작업

--- src/main/java/com/green/greengram/application/user/UserController.java
+++ src/main/java/com/green/greengram/application/user/UserController.java
@@ -1,11 +1,15 @@
 package com.green.greengram.application.user;
 
+import com.green.greengram.application.user.model.UserSignUpReq;
 import com.green.greengram.config.model.ResultResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Slf4j
@@ -16,7 +20,9 @@ public class UserController {
 
 //   회원가입
     @PostMapping("/sign-up")
-    public ResultResponse signUp() {
+    public ResultResponse<?> signUp(@Valid @RequestPart UserSignUpReq req, @RequestPart(required = false) MultipartFile pic) {
+        log.info("req:{}", req);
+        log.info("pic:{}", pic != null ? pic.getOriginalFilename() : pic);
         return new ResultResponse<Integer>("", 1);
     }
 
---
+++ src/main/java/com/green/greengram/application/user/model/UserSignUpReq.java
@@ -0,0 +1,22 @@
+package com.green.greengram.application.user.model;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UserSignUpReq {
+//    @Size(min=4, max=50, message = "아이디는 4~50자까지 작성 가능합니다")  // 밸리데이션 기능
+    @Pattern(regexp = "^[a-zA-Z0-9_]{4,50}$", message = "아이디는 영어, 숫자, 언더바로만 구성되어야 하며 4~50자까지 작성할 수 있습니다.")
+    private String uid;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
+            message = "비밀번호는 영문/숫자/특수문자 최소 1개씩 포함, 8~16자로 설정해주세요. (특수문자 일부 제외)")
+    private String upw;
+
+    @Pattern(regexp = "^[가-힣]{2,10}$", message = "닉네임은 한글로 2~10자까지 입력 가능합니다." )
+    private String nickName;
+
+}
