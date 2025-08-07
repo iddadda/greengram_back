2f9f12024bc75182f454173fa48ef7aec8fe1502
iddadda <iddadda241@gmail.com>
2025-08-07 11:32:10 +0900

    회원가입 Validation 추가

--- src/main/java/com/green/greengram/application/user/model/UserSignUpReq.java
+++ src/main/java/com/green/greengram/application/user/model/UserSignUpReq.java
@@ -1,6 +1,7 @@
 package com.green.greengram.application.user.model;
 
 import com.green.greengram.config.enumcode.model.EnumUserRole;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.ToString;
@@ -11,9 +12,11 @@ import java.util.List;
 @ToString
 public class UserSignUpReq {
 //    @Size(min=4, max=50, message = "아이디는 4~50자까지 작성 가능합니다")  // 밸리데이션 기능
+    @NotNull(message = "아이디는 필수로 입력하셔야 합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9_]{4,50}$", message = "아이디는 영어, 숫자, 언더바로만 구성되어야 하며 4~50자까지 작성할 수 있습니다.")
     private String uid;
 
+    @NotNull(message = "비밀번호는 필수로 입력하셔야 합니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
             message = "비밀번호는 영문/숫자/특수문자 최소 1개씩 포함, 8~16자로 설정해주세요. (특수문자 일부 제외)")
     private String upw;
