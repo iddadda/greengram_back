db4231b6ee03a73b877401d34c1b69fad8713c9e
iddadda <iddadda241@gmail.com>
2025-08-11 17:23:38 +0900

    피드 좋아요 수정

--- src/main/java/com/green/greengram/application/feed/model/FeedGetRes.java
+++ src/main/java/com/green/greengram/application/feed/model/FeedGetRes.java
@@ -15,5 +15,6 @@ public class FeedGetRes {
     private String writerUid;
     private String writerNickName;
     private String writerPic;
+    private int isLike;  // 0 이면 좋아요 x, 1 이면 좋아요 o
     private List<String> pics;
 }
